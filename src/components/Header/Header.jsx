76742958f01242440990a91b617e3b70cfc6c978
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-03-07 22:23:45 +0530

    Header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header className="shadow sticky z-50 top-0">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div className="d-flex ">
+          <Link to="/" className="logo flex ">
+            <img
+              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+              className="mr-3 h-12"
+              alt=""
+            />
+          </Link>
+          <div className="flex items-center lg:order-2">
+            <Link
+              to="#"
+              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+            >
+              Login
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
