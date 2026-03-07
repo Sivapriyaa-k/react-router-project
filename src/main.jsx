6e031d7e4642803b885041251c8d62b593e41519
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-03-07 21:51:23 +0530

    layout updated

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,19 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Layout />}></Route>),
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>,
+  ),
 );
-ReactDom.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </StrictMode>,
 );
