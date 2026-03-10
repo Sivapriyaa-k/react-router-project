d702f79d282b65749f83e188bee7ac9c1c8973fa
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-03-10 21:57:00 +0530

    Header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -51,12 +51,12 @@ function Header() {
               </li>
               <li>
                 <NavLink
-                  to="/"
+                  to="/contact"
                   className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
-                  Home
+                  Contact
                 </NavLink>
               </li>
               <li>
