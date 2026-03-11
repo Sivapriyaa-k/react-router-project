662a3bfb9e61383464f659ced6430463ca3789cc
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-03-11 21:32:22 +0530

    Header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ function Header() {
             </Link>
             <Link
               to="#"
-              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className="bg-red-700 text-white hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Get Started
             </Link>
